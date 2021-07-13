7701a5c2a6317355b8a3733029f8f0affe3a781b
Tahirli Tural Atabey <Tahi_ri96@itstep.edu.az>
2021-07-13 14:51:50 +0400

    Add project files.

---
+++ PubMVVM/Repostory/FakeRepo.cs
@@ -0,0 +1,43 @@
+﻿using PubMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubMVVM.Repostory
+{
+    public class FakeRepo
+    {
+        public List<Pub> GetAll()
+        {
+            return new List<Pub>
+            {
+                new Pub
+                {
+                    Name="NZS",
+                    Price=1,
+                    Volume=0.4
+                },
+                new Pub
+                {
+                    Name="Blanche",
+                    Price=7,
+                    Volume=0.4
+                },
+                new Pub
+                {
+                    Name="Erdinger",
+                    Price=7,
+                    Volume=0.5
+                },
+                new Pub
+                {
+                    Name="Salyan",
+                    Price=1,
+                    Volume=0.4
+                }
+            };
+        }
+    }
+}
---
+++ PubMVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PubMVVM.Model;
+using PubMVVM.Repostory;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubMVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainWindow MainWindow { get; set; }
+        public FakeRepo PubRepostory { get; set; }
+        public ObservableCollection<Pub> Pubs { get; set; }
+        public MainViewModel()
+        {
+            PubRepostory = new FakeRepo();
+            Pubs = new ObservableCollection<Pub>(PubRepostory.GetAll());
+        }
+    }
+}

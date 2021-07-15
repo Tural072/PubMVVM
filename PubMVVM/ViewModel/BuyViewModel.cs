aadbd7fe61ba916e6024e01bf431725a0b36bebc
Tural <tural.spotify.com@gmail.com>
2021-07-15 14:42:15 +0400

    I add RelayCommand and BuyWindow

---
+++ PubMVVM/ViewModel/BuyViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PubMVVM.Model;
+using PubMVVM.Repostory;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubMVVM.ViewModel
+{
+    public class BuyViewModel:BaseViewModel
+    {
+        private Pub Pub;
+
+        public Pub MyProperty
+        {
+            get { return Pub; }
+            set { Pub = value;OnPropertyChanged();}
+        }
+
+       
+    }
+}
--- PubMVVM/ViewModel/MainViewModel.cs
+++ PubMVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using PubMVVM.Model;
 using PubMVVM.Repostory;
+using PubMVVM.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,15 +10,35 @@ using System.Threading.Tasks;
 
 namespace PubMVVM.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel:BaseViewModel
     {
         public MainWindow MainWindow { get; set; }
         public FakeRepo PubRepostory { get; set; }
         public ObservableCollection<Pub> Pubs { get; set; }
+        private Pub pub;
+
+        public Pub Pub
+        {
+            get { return pub; }
+            set { pub = value; OnPropertyChanged(); }
+        }
+        public RelayCommand ShowHistoryCommand { get; set; }
+        public RelayCommand BuyCommand { get; set; }
+        public RelayCommand ResetCommand { get; set; }
+        public RelayCommand SelectedItemChangedCommand { get; set; }
+        public BuyWindow BuyWindow { get; set; }
         public MainViewModel()
         {
             PubRepostory = new FakeRepo();
             Pubs = new ObservableCollection<Pub>(PubRepostory.GetAll());
+
+            SelectedItemChangedCommand = new RelayCommand((SelectedItem) =>
+            {
+                var item = SelectedItem as Pub;
+                Pub = item;
+                //MainWindow.Mimage.Source = new BitmapImage(new Uri(
+                // Pub.ImagePath, UriKind.RelativeOrAbsolute));
+            });
         }
     }
 }
